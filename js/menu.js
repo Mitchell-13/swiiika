f1b2aff4ee15f7ff2aca70a6b3287b0b70aba817
Mitchell Ogden <mitchellogden13@gmail.com>
2023-05-10 19:09:17 -0600

    making more responsive

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,23 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+
+  function removeClassOnResize() {
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var business = document.getElementById('businesses');
+    var influencer = document.getElementById('influencers')
+    if (windowWidth < 760) {
+      business.classList.remove('from-left', 'btn');
+      influencer.classList.remove('from-right', 'btn');
+    } else {
+      business.classList.add('from-left','btn');
+      influencer.classList.add('from-right','btn');
+    }
+  }
+  
+  // Call the function on window resize
+  window.addEventListener('resize', removeClassOnResize);
+  
+  // Call the function on page load
+  window.addEventListener('load', removeClassOnResize);
