db042b206c66d10d6562763977156dd6ca3cc818
Mitchell <mitchellogden13@gmail.com>
2023-05-21 13:43:22 -0600

    fix

---
+++ js/clients.js
@@ -0,0 +1,28 @@
+function getElementHeightWithMargin(element) {
+    var computedStyle = window.getComputedStyle(element);
+    var height = element.offsetHeight;
+    var marginTop = parseFloat(computedStyle.marginTop);
+    var marginBottom = parseFloat(computedStyle.marginBottom);
+  
+    var totalHeight = height + marginTop + marginBottom;
+  
+    return totalHeight;
+  }
+
+
+function resizeElementHeight() {
+    var element = document.getElementById("clients");
+
+    var box = document.querySelector(".team");
+    var height = getElementHeightWithMargin(box);
+  
+    // Adjust the height of the element
+    element.style.height = height + "px";
+  }
+  
+  // Call the function when the document loads
+  window.onload = resizeElementHeight;
+  
+  // Call the function when the window is resized
+  window.onresize = resizeElementHeight;
+  
